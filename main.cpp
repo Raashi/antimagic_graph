9ed389f12321d6c859bfaba328d8fab46cb6b5e8
Ivan Podkidyshev <raashicat@gmail.com>
2019-03-08 12:24:50 +0400

    Started developing main.cpp as it should be

--- main.cpp
+++ main.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 #include "perms.h"
 #include "graph.h"
 
 
-int main() {
-    /*
-    std::cout << "Enter number of permutations: ";
-    int n;
-    std::cin >> n;
+int main(int argc, char **argv) {
+    if (argc < 2)
+        throw -1;
 
-    PermGen gen{};
-    int* perm = init_gen(gen, n);
-    while (perm != nullptr) {
-        print_perm(perm, n);
-        perm = next(gen);
+    std::string op = argv[1];
+    if (op == "brute") {
+        //
+    } else if (op == "g6") {
+        if (argc == 2)
+            throw 1;
+
+        std::string op_g6 = argv[2];
+        if (op_g6 == "to") {
+            int n;
+            std::cout << "Enter number of edges: ";
+            std::cin >> n;
+            Edges edges;
+            std::cout << "Enter n edges:\n";
+            for (int i = 0; i < n; ++i) {
+                int a, b;
+                std::cin >> a >> b;
+                edges.emplace_back(a, b);
+            }
+
+        } else if (op_g6 == "from") {
+            std::string graph6;
+            std::cout << "Enter graph in graph6 format: ";
+            std::cin >> graph6;
+            Graph(graph6).display();
+        } else
+            throw 1;
+    } else {
+        throw 1;
     }
-     */
-    // DQc
-
-    // std::cout << "Enter graph6: ";
-    std::string graph6 = "DQc";
-    // std::cin >> graph6;
-
-    Graph graph(graph6);
-    graph.display();
-
-    std::cout << std::endl;
-    std::cout << graph.is_antimagic();
 
     return 0;
 }
